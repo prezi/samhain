a8d08df6417bd6085aa17fa73dcae4c7172d7915
ele mooney <ele.mooney@socrata.com>
2015-11-13 14:45:19 -0800

    cookbook init

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'chef-samhain'
+maintainer       'Ele Mooney'
+maintainer_email 'ele.mooney@socrata.com'
+license          'All rights reserved'
+description      'Installs/Configures chef-samhain'
+long_description 'Installs/Configures chef-samhain'
+version          '0.1.0'
+
+supports 'ubuntu'
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: chef-samhain
+# Recipe:: default
+#
+# installs and configures samhain for host integrity monitoring
+
+# get the package and test gpg
+
+# build 
+
+# install
